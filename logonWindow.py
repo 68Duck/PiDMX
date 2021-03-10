3bde0458ac7e1026693c4923142a7b0e508e68db
68Duck <Joshuahenry111@yahoo.co.uk>
2021-03-10 13:07:17 +0000

    Updated login window.

--- logonWindow.py
+++ logonWindow.py
@@ -29,6 +29,10 @@ class LogonWindow(QMainWindow,uic.loadUiType("logon.ui")[0]):
         self.initUI()
 
     def initUI(self):
+        self.usernameInput.setPlaceholderText("Username")
+        self.passwordInput.setPlaceholderText("Password")
+        self.createUsernameInput.setPlaceholderText("New Username")
+        self.createPasswordInput.setPlaceholderText("New Password")
         self.createPasswordInput.setEchoMode(QLineEdit.Password)
         self.passwordInput.setEchoMode(QLineEdit.Password)
         self.submitButton.clicked.connect(self.submitButtonPressed)
@@ -107,78 +111,3 @@ class LogonWindow(QMainWindow,uic.loadUiType("logon.ui")[0]):
         self.lightDisplay = LightDisplayWindow(self.lightDisplay,self.dataBaseManager)
         self.app.installEventFilter(self.lightDisplay)
         self.lightDisplay.show()
-
-
-
-
-# class LogonWindow(QWidget):
-#     def __init__(self,lightDisplay):
-#         super().__init__()
-#         self.lightDisplay = lightDisplay
-#         self.logonValid = False
-#         self.logonDatabaseManager = DataBaseManager("logon.db")
-#         self.setGeometry(200,100,500,500)
-#         self.setWindowTitle("Log on")
-#         self.initUI()
-#         self.show()
-#
-#     def initUI(self):
-#         self.usernameInput = QLineEdit(self)
-#         self.usernameInput.setPlaceholderText("Enter Username")
-#         self.usernameInput.move(50,50)
-#
-#         self.passwordInput = QLineEdit(self)
-#         self.passwordInput.setPlaceholderText("Enter Password")
-#         self.passwordInput.move(50,100)
-#
-#         self.submitButton = QtWidgets.QPushButton(self)
-#         self.submitButton.move(50,150)
-#         self.submitButton.setText("Submit")
-#         self.submitButton.clicked.connect(self.submitButtonPressed)
-#
-#         self.createAccountButton = QPushButton(self)
-#         self.createAccountButton.move(50,200)
-#         self.createAccountButton.setText("Create new account")
-#         self.createAccountButton.clicked.connect(self.createAccountButtonClicked)
-#
-#     def keyPressEvent(self,e):
-#         if e.key() == Qt.Key_Return:
-#             self.submitButtonPressed()
-#
-#     def createAccountButtonClicked(self):
-#         self.createAccountWindow = CreateAccountWindow(self.logonDatabaseManager,self)
-#         self.createAccountWindow.show()
-#
-#     def createDatabase(self,databaseID):
-#         self.dataBaseManager = DataBaseManager("dmx"+str(databaseID)+".db")
-#         self.dataBaseManager.createLightingRigsTable()
-#         self.dataBaseManager.createMainPlaybackTable()
-#         self.dataBaseManager.createMainSequenceTable()
-#
-#     def submitButtonPressed(self):
-#         username = self.usernameInput.text()
-#         password = self.passwordInput.text()
-#         self.logonValid = self.passwordValidation(username,password)
-#         if self.logonValid is not False:
-#             self.databaseID = self.logonValid
-#             if self.databaseID == 0:
-#                 self.dataBaseManager = DataBaseManager("dmx.db")
-#             else:
-#                 self.dataBaseManager = DataBaseManager("dmx"+str(self.databaseID)+".db")
-#             self.openLightDisplay()
-#         else:
-#             self.errorWindow = ErrorWindow("The username or password is not valid. Please try again. Logon is case sensitive")
-#             self.usernameInput.setText("")
-#             self.passwordInput.setText("")
-#
-#     def passwordValidation(self,username,password):
-#         logons = self.logonDatabaseManager.getAllData("logon")
-#         for logon in logons:
-#             if logon[1] == username:
-#                 if logon[2] == password:
-#                     return logon[3]
-#         return False
-#
-#     def openLightDisplay(self):
-#         self.lightDisplay = LightDisplayWindow(self.lightDisplay,self.dataBaseManager)
-#         self.lightDisplay.show()
