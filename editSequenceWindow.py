94a4b4346c47458e812cf92677abb4f1536384c4
68Duck <Joshuahenry111@yahoo.co.uk>
2021-04-01 21:31:55 +0100

    Added ability to remove playback in sequence with checkbox

--- editSequenceWindow.py
+++ editSequenceWindow.py
@@ -15,40 +15,88 @@ class EditSequenceWindow(QWidget,uic.loadUiType("editSequenceWindow.ui")[0]):
         self.setWindowTitle("Edit Sequence")
         # self.setGeometry(250,250,300,250)  #x,y,width,height
 
-        self.sequenceToOpen = self.dataBaseManager.getAllData("sequence"+str(self.sequenceWindow.sequenceID))
         self.initUI()
     def initUI(self):
-        # self.updateButton = QPushButton(self)
-        # self.updateButton.move(185,220)
-        self.updateButton.clicked.connect(self.updateButtonClicked)
-        # self.updateButton.setText("Update Sequence")
-        # self.updateButton.setFixedWidth(100)
-
-        # self.table = QTableWidget(self)
-        self.table.setRowCount(len(self.sequenceToOpen))
-        header = self.table.horizontalHeader()
+        self.sequenceToOpen = self.dataBaseManager.getAllData("sequence"+str(self.sequenceWindow.sequenceID))
+        # layout = QHBoxLayout()
+        # self.setLayout(layout)
+        self.tableWidget = TableWidget()
+        self.layout.addWidget(self.tableWidget)
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setHorizontalHeaderLabels(["number","time delay","select"])
+        # self.table.hide()
+        self.tableWidget.setRowCount(len(self.sequenceToOpen))
+        header = self.tableWidget.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
-        # self.table.setColumnCount(2)
-        # self.table.setHorizontalHeaderLabels(["number","time delay"])
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
         xPos = 0
         yPos = 0
+        self.checkBoxes = []
         for i in range(len(self.sequenceToOpen)):
             id = QTableWidgetItem(str(self.sequenceToOpen[i][0]))
-            self.table.setItem(i,0,id)  #in the from x,y,item
+            self.tableWidget.setItem(i,0,id)  #in the from x,y,item
             timeDelay = QTableWidgetItem(str(self.sequenceToOpen[i][2]))
-            # print(self.sequenceToOpen[i][2])
-            self.table.setItem(i,1,timeDelay)  #in the from x,y,item
+            self.tableWidget.setItem(i,1,timeDelay)  #in the from x,y,item
+            # self.checkBox = QCheckBox(self)
+            chkBoxItem = QTableWidgetItem()
+            chkBoxItem.setFlags(Qt.ItemIsUserCheckable | Qt.ItemIsEnabled)
+            chkBoxItem.setTextAlignment(Qt.AlignCenter)
+            chkBoxItem.setCheckState(Qt.Unchecked)
 
-        self.table.resizeRowsToContents()
-        self.table.resizeColumnsToContents()
+            self.tableWidget.setItem(i,2,chkBoxItem)
+            self.checkBoxes.append(chkBoxItem)
+
+        self.tableWidget.resizeRowsToContents()
+        self.tableWidget.resizeColumnsToContents()
+
+        self.updateButton.clicked.connect(self.updateButtonClicked)
+        self.deleteButton.clicked.connect(self.deleteButtonClicked)
+
+    def deleteButtonClicked(self):
+        for row in range(len(self.sequenceToOpen)):
+                itemArray = []
+                for row in range(len(self.sequenceToOpen)):
+                    array = []
+                    if self.tableWidget.item(row,2).checkState():
+                        pass
+                    else:
+                        for column in range(2):
+                            item = self.tableWidget.item(row,column).text()
+                            try:
+                                item = float(item)
+                            except:
+                                self.errorWindow = ErrorWindow("The values need to be numbers. Please try again")
+                                return
+                            array.append(item)
+                        array.insert(0,row+1)
+                        itemArray.append(array)
+                for i in range(len(itemArray)):
+                    itemArray[i][0] = i+1
+                for arr in itemArray:
+                    for sequence in self.sequenceToOpen:
+                        if float(sequence[0]) == float(arr[0]):
+                            arr.insert(1,sequence[1])
+                itemArray = sortByTerm(itemArray,2)  #so sort by third as 0 indexed
+                for arr in itemArray:
+                    arr.pop(2)
+                for i in range(len(itemArray)):
+                    itemArray[i][0] = i+1
+                self.dataBaseManager.createSequenceTable(self.sequenceWindow.sequenceID)  #creates a blank table
+                for arr in itemArray:
+                    record = [None,arr[1],arr[2]]
+                    self.dataBaseManager.insertRecord("sequence"+str(self.sequenceWindow.sequenceID),record)
+        self.tableWidget.hide()
+        self.initUI()
+            # print(self.tableWidget.item(row,2).checkState())
+
 
     def updateButtonClicked(self):
         itemArray = []
         for row in range(len(self.sequenceToOpen)):
             array = []
             for column in range(2):
-                item = self.table.item(row,column).text()
+                item = self.tableWidget.item(row,column).text()
                 try:
                     item = float(item)
                 except:
@@ -73,3 +121,60 @@ class EditSequenceWindow(QWidget,uic.loadUiType("editSequenceWindow.ui")[0]):
             record = [None,arr[1],arr[2]]
             self.dataBaseManager.insertRecord("sequence"+str(self.sequenceWindow.sequenceID),record)
         self.close()
+
+
+class TableWidget(QTableWidget):  #taken from https://stackoverflow.com/questions/26227885/drag-and-drop-rows-within-qtablewidget
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
+        self.viewport().setAcceptDrops(True)
+        self.setDragDropOverwriteMode(False)
+        self.setDropIndicatorShown(True)
+
+        self.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        self.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.setDragDropMode(QAbstractItemView.InternalMove)
+
+        self.setStyleSheet("background-color:white;")
+
+    def dropEvent(self, event: QDropEvent):
+        if not event.isAccepted() and event.source() == self:
+            drop_row = self.drop_on(event)
+
+            rows = sorted(set(item.row() for item in self.selectedItems()))
+            rows_to_move = [[QTableWidgetItem(self.item(row_index, column_index)) for column_index in range(self.columnCount())]
+                            for row_index in rows]
+            for row_index in reversed(rows):
+                self.removeRow(row_index)
+                if row_index < drop_row:
+                    drop_row -= 1
+
+            for row_index, data in enumerate(rows_to_move):
+                row_index += drop_row
+                self.insertRow(row_index)
+                for column_index, column_data in enumerate(data):
+                    self.setItem(row_index, column_index, column_data)
+            event.accept()
+            for row_index in range(len(rows_to_move)):
+                self.item(drop_row + row_index, 0).setSelected(True)
+                self.item(drop_row + row_index, 1).setSelected(True)
+        super().dropEvent(event)
+
+    def drop_on(self, event):
+        index = self.indexAt(event.pos())
+        if not index.isValid():
+            return self.rowCount()
+
+        return index.row() + 1 if self.is_below(event.pos(), index) else index.row()
+
+    def is_below(self, pos, index):
+        rect = self.visualRect(index)
+        margin = 2
+        if pos.y() - rect.top() < margin:
+            return False
+        elif rect.bottom() - pos.y() < margin:
+            return True
+        # noinspection PyTypeChecker
+        return rect.contains(pos, True) and not (int(self.model().flags(index)) & Qt.ItemIsDropEnabled) and pos.y() >= rect.center().y()
