a50580eaf3da8ac9f501f880a9bf3e1e37fab95b
68Duck <Joshuahenry111@yahoo.co.uk>
2021-10-29 11:43:41 +0100

    Updated the serial class so it will send the data through faster

--- PiDMX/pydmx.py
+++ PiDMX/pydmx.py
@@ -6,7 +6,7 @@ from PyQt5.QtWidgets import*
 from PyQt5.QtGui import*
 from PyQt5.QtCore import*
 import random
-
+#
 # class PyDMX:  ##Coppied from the internet. This does the sending to dmx stuff
 #     def __init__(self,COM='COM8',Brate=250000,Bsize=8,StopB=2):
 #         #start serial
@@ -52,12 +52,13 @@ import random
 
 class PyDMX(object):
     def __init__(self,port="COM3"):
-        self.universeData = [0]*1025  #the first item is the start bit so should not change
+        self.universeData = [0]*512  #the first item is the start bit so should not change
         baudrate = 250000
         try:
-            self.ser = serial.Serial(port,baudrate)
+            self.ser = serial.Serial(port,baudrate,bytesize=8,stopbits=2)
             self.working = True
-        except:
+        except Exception as e:
+            print(e)
             self.working = False
             raise Exception("The serial is not working. Please make sure the lights are plugged in and that you don't have any other dmx scripts running and make sure the port is correct")
 
@@ -74,18 +75,21 @@ class PyDMX(object):
         if id>512 or id<1:
             raise Exception("The id value needs to be between 1 and 512 inclusive.")
             return
-        self.universeData[id*2] = data
+        self.universeData[id] = data
 
     def send(self,universeData = None):
         if universeData is None:
             pass
         else:
             self.universeData = universeData
-        self.ser.send_break(duration = 88/1000000)
-        time.sleep(2/1000000)
-        self.ser.write(0)
-        self.ser.write(bytearray(self.universeData))
-        time.sleep(55/1000)
+        self.ser.send_break(duration=92/1000000)
+        time.sleep(12/1000000.0)
+        try:
+            self.ser.write(0)
+            self.ser.write(bytearray(self.universeData))
+        except:
+            raise Excpetion("The serial cannot write. Check the cable is plugged in.")
+        time.sleep(10/1000)
 
     def send_zero(self):
         universeData = [0]*1025
